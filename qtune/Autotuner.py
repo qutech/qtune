15afc5815dc9330e782f0266e4f076645d45a261
INSTITUT2B\teske <julian.teske@rwth-aachen.de>
2017-12-12 14:48:07 +0100

    -Reimplement gradient calculation wich now also etimates the standard deviation of the gradient and the evaluations -add tunable gates -add function to initialize the GradKalman of the ChargeDiagram -minor fixes

--- qtune/Autotuner.py
+++ qtune/Autotuner.py
@@ -1,12 +1,13 @@
 import pandas as pd
 import numpy as np
+import pickle
 from typing import Tuple
 from qtune.experiment import Experiment
 from qtune.Evaluator import Evaluator
 from qtune.Solver import Solver
 from qtune.Basic_DQD import BasicDQD
 from qtune.chrg_diag import ChargeDiagram
-from qtune.Kalman_heuristics import load_charge_diagram_covariance_noise_from_histogram, \
+from qtune.Kalman_heuristics import load_charge_diagram_gradient_covariance_noise_from_histogram, \
     save_charge_diagram_histogram_position_gradient
 
 
@@ -20,6 +21,10 @@ class Autotuner:
         for e in evaluators:
             self.add_evaluator(e)
         self._desired_values = desired_values
+        self.tunable_gates = self.experiment.read_gate_voltages()
+        self.gradient = None
+        self.gradient_std = None
+        self.evaluation_std = None
 
     @property
     def evaluators(self):
@@ -47,26 +52,32 @@ class Autotuner:
     def desired_values(self, desired_values: pd.Series):
         assert(desired_values.index.tolist() == self.parameters.index.tolist())
         self._desired_values = desired_values
+        if self.solver is not None:
+            self.solver.desired_values = desired_values
 
     def set_desired_values_manually(self):
         raise NotImplementedError
 
-    def evaluate_gradient(self, delta_u=4e-3, n_repetitions=3) -> pd.DataFrame:
+    def set_gate_voltages(self, new_voltages):
+        self.experiment.set_gate_voltages(new_gate_voltages=new_voltages)
+
+    def evaluate_gradient_covariance_noise(self, delta_u=4e-3, n_repetitions=3, save_to_file: bool = False,
+                                           filename: str = None) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series]:
         current_gate_positions = self.experiment.read_gate_voltages()
-        gradient = pd.DataFrame()
-        for gate in self.experiment.gate_voltage_names:
-            positive_detune_parameter = pd.Series()
-            negative_detune_parameter = pd.Series()
+        positive_detune = pd.DataFrame()
+        negative_detune = pd.DataFrame()
+        positive_detune_parameter = pd.Series()
+        negative_detune_parameter = pd.Series()
+        for gate in self.tunable_gates.index.tolist():
+            for parameter in self.parameters.index.tolist():
+                positive_detune_parameter[parameter] = np.zeros((n_repetitions,), dtype=float)
+                negative_detune_parameter[parameter] = np.zeros((n_repetitions,), dtype=float)
 
-            for i in self.parameters.index.tolist():
-                positive_detune_parameter[i] = np.zeros((n_repetitions,), dtype=float)
-                negative_detune_parameter[i] = np.zeros((n_repetitions,), dtype=float)
+            detuning = pd.Series((delta_u, ), (gate, ))
+            new_gate_positions = current_gate_positions.add(detuning, fill_value=0)
+            self.set_gate_voltages(new_gate_positions)
 
-            detuning = pd.Series(delta_u, gate)
-
-            for i in range(1, n_repetitions):
-                new_gate_positions = current_gate_positions.add(detuning, fill_value=0)
-                self.experiment.set_gate_voltages(new_gate_positions)
+            for i in range(n_repetitions):
 
                 for e in self.evaluators:
                     evaluation_result = e.evaluate()
@@ -81,8 +92,11 @@ class Autotuner:
                     for r in evaluation_result.index.tolist():
                         positive_detune_parameter[r][i] = evaluation_result[r]
 
-                new_gate_positions = current_gate_positions.add(-1.*detuning, fill_value=0)
-                self.experiment.set_gate_voltages(new_gate_positions)
+            self.set_gate_voltages(current_gate_positions)
+            new_gate_positions = current_gate_positions.add(-1.*detuning, fill_value=0)
+            self.set_gate_voltages(new_gate_positions)
+
+            for i in range(n_repetitions):
 
                 for e in self.evaluators:
                     evaluation_result = e.evaluate()
@@ -97,19 +111,46 @@ class Autotuner:
                     for r in evaluation_result.index.tolist():
                         negative_detune_parameter[r][i] = evaluation_result[r]
 
-            for i in self.parameters.index.tolist():
-                positive_detune_parameter[i] = np.nanmean(positive_detune_parameter[i])
-                negative_detune_parameter[i] = np.nanmean(negative_detune_parameter[i])
-            gradient_column = (positive_detune_parameter.add(-1.*negative_detune_parameter))/2./delta_u
-            if gradient.empty:
-                gradient = gradient_column
-            else:
-                gradient = gradient.join(gradient_column)
-            self.experiment.set_gate_voltages(current_gate_positions)
-        return gradient
+            self.set_gate_voltages(current_gate_positions)
 
-    def set_solver(self):
-        raise NotImplementedError
+            positive_detune_parameter_df = positive_detune_parameter.to_frame(gate)
+            if positive_detune.empty:
+                positive_detune = positive_detune_parameter_df
+            else:
+                positive_detune = positive_detune_parameter_df.join(positive_detune_parameter)
+            negative_detune_parameter_df = negative_detune_parameter.to_frame(gate)
+            if negative_detune.empty:
+                negative_detune = negative_detune_parameter_df
+            else:
+                negative_detune = negative_detune_parameter_df.join(positive_detune_parameter)
+
+        gradient = (positive_detune - negative_detune) / 2. / delta_u
+        gradient_std = gradient.apply(np.nanstd)
+        gradient = gradient.apply(np.nanmean)
+        evaluation_std = positive_detune_parameter.apply(np.nanstd)
+
+        if save_to_file:
+            save_data = pd.Series([gradient, gradient_std, evaluation_std],
+                                  ['gradient', 'gradient_std', 'evaluation_std'])
+            with open(filename, 'wb') as handle:
+                pickle.dump(save_data, handle, protocol=pickle.HIGHEST_PROTOCOL)
+
+        self.gradient = gradient
+        self.gradient_std = gradient_std
+        self.evaluation_std = evaluation_std
+
+        return gradient, gradient_std, evaluation_std
+
+    def load_gradient_data(self, filename: str) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series]:
+        with open(filename, 'rb') as handle:
+            data = pickle.load(handle)
+        gradient = data['gradient']
+        gradient_std = data['gradient_std']
+        evaluation_std = data['evaluation_std']
+        return gradient, gradient_std, evaluation_std
+
+    def set_solver(self, solver: Solver):
+        self.solver = solver
 
     def autotune(self):
         raise NotImplementedError
@@ -121,8 +162,12 @@ class ChargeDiagramAutotuner(Autotuner):
                  charge_diagram_covariance=None, charge_diagram_noise=None):
         super().__init__(experiment=dqd, solver=solver, evaluators=evaluators, desired_values=desired_values)
         self.charge_diagram = ChargeDiagram(dqd=dqd)
-        self.charge_diagram.initialize_kalman(initX=charge_diagram_gradient, initP=charge_diagram_covariance,
-                                              initR=charge_diagram_noise)
+        self.tunable_gates = self.tunable_gates.drop(['RFA', 'RFB', 'BA', 'BB'])
+        if charge_diagram_gradient is not None or charge_diagram_covariance is not None or charge_diagram_noise is not None:
+            self.initialize_charge_diagram_kalman(charge_diagram_gradient=charge_diagram_gradient,
+                                                  charge_diagram_covariance=charge_diagram_covariance,
+                                                  charge_diagram_noise=charge_diagram_noise,
+                                                  heuristic_measurement=False, save_to_file=False, load_file=False)
 
     def initialize_charge_diagram_kalman(self, charge_diagram_gradient=None, charge_diagram_covariance=None,
                                          charge_diagram_noise=None, heuristic_measurement: bool = False, n_noise=15,
@@ -141,17 +186,22 @@ class ChargeDiagramAutotuner(Autotuner):
             if filename is None:
                 print('Please insert a file name!')
                 return
-            charge_diagram_covariance, charge_diagram_noise = load_charge_diagram_covariance_noise_from_histogram(
-                filename)
+            charge_diagram_gradient, charge_diagram_covariance, charge_diagram_noise = \
+                load_charge_diagram_gradient_covariance_noise_from_histogram(filename)
         self.charge_diagram.initialize_kalman(initX=charge_diagram_gradient, initP=charge_diagram_covariance,
                                               initR=charge_diagram_noise)
 
+    def set_gate_voltages(self, new_voltages):
+        self.experiment.set_gate_voltages(new_gate_voltages=new_voltages)
+        self.charge_diagram.center_diagram()
+
     def measure_charge_diagram_histogram(self, n_noise=30, n_cov=30, save_to_file=True, filename: str = None) -> Tuple[
-        np.array, np.array]:
+        np.array, np.array, np.array]:
         position_histo, grad_histo = save_charge_diagram_histogram_position_gradient(ch_diag=self.charge_diagram,
                                                                                      n_noise=n_noise, n_cov=n_cov,
                                                                                      savetofile=save_to_file,
                                                                                      filename=filename)
+        gradient = np.nanmean(grad_histo)
         std_position = np.std(position_histo, 0)
         std_grad = np.std(grad_histo, 0)
         heuristic_covariance = np.zeros((4, 4))
@@ -162,67 +212,5 @@ class ChargeDiagramAutotuner(Autotuner):
         heuristic_noise = np.zeros((2, 2))
         heuristic_noise[0, 0] = (2. * std_position[0]) * (2. * std_position[0])
         heuristic_noise[1, 1] = (2. * std_position[1]) * (2. * std_position[1])
-        return heuristic_covariance, heuristic_noise
+        return gradient, heuristic_covariance, heuristic_noise
 
-    def evaluate_gradient(self, delta_u=4e-3, n_repetitions=3) -> pd.DataFrame:
-        current_gate_positions = self.experiment.read_gate_voltages()
-        gradient = pd.DataFrame()
-        tunable_gates = current_gate_positions.drop(['RFA', 'RFB', 'BA', 'BB'])
-        for gate in tunable_gates.index.tolist():
-            positive_detune_parameter = pd.Series()
-            negative_detune_parameter = pd.Series()
-
-            for parameter in self.parameters.index.tolist():
-                positive_detune_parameter[parameter] = np.zeros((n_repetitions,), dtype=float)
-                negative_detune_parameter[parameter] = np.zeros((n_repetitions,), dtype=float)
-
-            detuning = pd.Series((delta_u, ), (gate, ))
-
-            for i in range(n_repetitions):
-                new_gate_positions = current_gate_positions.add(detuning, fill_value=0)
-                self.experiment.set_gate_voltages(new_gate_positions)
-                self.charge_diagram.center_diagram()
-
-                for e in self.evaluators:
-                    evaluation_result = e.evaluate()
-
-                    if evaluation_result['failed']:
-                        evaluation_result = evaluation_result.drop(['failed'])
-                        for r in evaluation_result.index.tolist():
-                            evaluation_result[r] = np.nan
-                    else:
-                        evaluation_result = evaluation_result.drop(['failed'])
-
-                    for r in evaluation_result.index.tolist():
-                        positive_detune_parameter[r][i] = evaluation_result[r]
-                self.experiment.set_gate_voltages(current_gate_positions)
-                self.charge_diagram.center_diagram()
-                new_gate_positions = current_gate_positions.add(-1.*detuning, fill_value=0)
-                self.experiment.set_gate_voltages(new_gate_positions)
-                self.charge_diagram.center_diagram()
-
-                for e in self.evaluators:
-                    evaluation_result = e.evaluate()
-
-                    if evaluation_result['failed']:
-                        evaluation_result = evaluation_result.drop(['failed'])
-                        for r in evaluation_result.index.tolist():
-                            evaluation_result[r] = np.nan
-                    else:
-                        evaluation_result = evaluation_result.drop(['failed'])
-
-                    for r in evaluation_result.index.tolist():
-                        negative_detune_parameter[r][i] = evaluation_result[r]
-                self.experiment.set_gate_voltages(current_gate_positions)
-                self.charge_diagram.center_diagram()
-
-            for i in self.parameters.index.tolist():
-                positive_detune_parameter[i] = np.nanmean(positive_detune_parameter[i])
-                negative_detune_parameter[i] = np.nanmean(negative_detune_parameter[i])
-            gradient_column = (positive_detune_parameter.add(-1.*negative_detune_parameter))/2./delta_u
-            gradient_column = gradient_column.to_frame(gate)
-            if gradient.empty:
-                gradient = gradient_column
-            else:
-                gradient = gradient.join(gradient_column)
-        return gradient
--- qtune/Kalman_heuristics.py
+++ qtune/Kalman_heuristics.py
@@ -14,8 +14,9 @@ def save_charge_diagram_histogram_position_gradient(ch_diag: ChargeDiagram, n_no
     for i in range(0, n_cov):
         grad_histo[i] = ch_diag.calculate_gradient()
 
-    std_position = np.std(position_histo, 0)
-    std_grad = np.std(grad_histo, 0)
+    std_position = np.nanstd(position_histo, 0)
+    std_grad = np.nanstd(grad_histo, 0)
+    gradient = np.nanmean(grad_histo, 0)
 
     if savetofile:
         data = {'position_histo': position_histo, 'grad_histo': grad_histo, 'std_position': std_position,
@@ -24,13 +25,14 @@ def save_charge_diagram_histogram_position_gradient(ch_diag: ChargeDiagram, n_no
         with open(filename, 'wb') as handle:
             pickle.dump(data, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-    return position_histo, grad_histo
+    return gradient, position_histo, grad_histo
 
 
-def load_charge_diagram_covariance_noise_from_histogram(
+def load_charge_diagram_gradient_covariance_noise_from_histogram(
         filename=r'Y:\GaAs\Autotune\Data\UsingPython\heuristics_kalman\NoiseCov.pickle'):
     with open(filename, 'rb') as handle:
         data = pickle.load(handle)
+    gradient = np.nanmean(data['grad_histo'], axis=0)
     initP = np.zeros((4, 4))
     std_grad = data["std_grad"]
     initP[0, 0] = 2. * std_grad[0, 0]
@@ -41,4 +43,4 @@ def load_charge_diagram_covariance_noise_from_histogram(
     std_position = data["std_position"]
     initR[0, 0] = (2. * std_position[0]) * (2. * std_position[0])
     initR[1, 1] = (2. * std_position[1]) * (2. * std_position[1])
-    return initP, initR
+    return gradient, initP, initR
