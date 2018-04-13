7728dff8209487db1b32d7f477c0e83f0dd79f52
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2018-04-13 14:05:19 +0200

    Solver interface

--- qtune/solver.py
+++ qtune/solver.py
@@ -1,136 +1,94 @@
+from typing import Tuple, Sequence, Optional
+
 import numpy as np
 import pandas as pd
-from typing import Tuple, Optional
-from qtune.kalman_gradient import KalmanGradient
-from qtune.evaluator import Evaluator
+
+import scipy.optimize
+
+from qtune.gradient import GradientEstimator
 
 
 class Solver:
     """
     The solver class implements an algorithm to minimise the difference of the parameters to the target values.
     """
-    def __init__(self, gate_names=None, gradient=None, evaluators: Tuple[Evaluator, ...] = (),
-                 desired_values: pd.Series = pd.Series()):
-        self.gradient = gradient
-        self.gate_names = gate_names
-        self.need_new_gradient = False
-        self.evaluators = ()
-        self.desired_values = desired_values.sort_index()
-        self.parameter = pd.Series()
-        for e in evaluators:
-            self.add_evaluator(e)
-        self.parameter.sort_index()
+    @property
+    def target(self) -> pd.Series:
+        raise NotImplementedError()
 
-    def check_dimensionality(self) -> bool:
-        if self.gradient.shape != (len(self.parameter.index.tolist()), len(self.gate_names)):
-            print('The gradients shape', self.gradient.shape, 'doesnt match the number of parameters',
-                  self.parameter.index.tolist(), 'and gate names', len(self.gate_names), '!')
-            return False
-        elif self.desired_values.index.tolist() != self.parameter.index.tolist():
-            print('The desired values', self.desired_values.index.tolist(), 'do not match the parameters',
-                  self.parameter.index.tolist(), '!')
-            return False
-        else:
-            return True
-
-    def add_evaluator(self, evaluator: Evaluator):
-        try:
-            self.parameter.append(evaluator.parameters, verify_integrity=True)
-        except ValueError:
-            print('A parameter of evaluator', evaluator, 'is already evaluated by another evaluator!')
-            return
-        self.evaluators += (evaluator, )
+    @target.setter
+    def target(self, val):
+        raise NotImplementedError()
 
     def suggest_next_step(self) -> pd.Series:
         raise NotImplementedError()
 
-    def update_after_step(self, voltages: pd.Series, measured_parameters: Optional[pd.Series]):
+    def update_after_step(self, voltages: pd.Series, parameters: pd.Series):
         raise NotImplementedError()
 
 
-class KalmanSolver(Solver):
-    """
-    Solver using the Kalman filter to update the gradient.
-    """
-    def __init__(self, gate_names=None, gradient=None, evaluators: Tuple[Evaluator, ...] = (),
-                 desired_values: pd.Series = pd.Series(), covariance=None, noise=None, alpha=1.02,
-                 shifting_uncertainty=None):
-        super().__init__(gate_names=gate_names, gradient=gradient, desired_values=desired_values, evaluators=evaluators)
-        self.covariance = covariance
-        self.noise = noise
-        self.shifting_uncertainty = shifting_uncertainty
-        if gradient is not None:
-            n_parameter, n_gates = gradient.shape()
-            self.grad_kalman = KalmanGradient(n_gates=n_gates,
-                                              n_params=n_parameter,
-                                              initial_gradient=gradient,
-                                              initial_covariance_matrix=covariance,
-                                              measurement_covariance_matrix=noise,
-                                              alpha=alpha)
+class NewtonSolver:
+    def __init__(self, target: pd.Series, gradient_estimators: Sequence[GradientEstimator],
+                 current_values: pd.Series=None):
+        self._target = target
+        self._gradient_estimators = list(gradient_estimators)
 
-    def initialize_kalman(self, gradient=None, covariance=None, noise=None, shifting_uncertainty=None, alpha=1.02):
-        if gradient is None:
-            gradient = self.gradient
-        n_parameters, n_gates = gradient.shape
-        self.grad_kalman = KalmanGradient(n_gates=n_gates,
-                                          n_params=n_parameters,
-                                          initial_gradient=gradient,
-                                          initial_covariance_matrix=covariance,
-                                          measurement_covariance_matrix=noise,
-                                          process_noise=shifting_uncertainty,
-                                          alpha=alpha)
-        self.gradient = gradient
+        self._current_position = current_values
 
-    def update_after_step(self, d_voltages_series: pd.Series, d_parameter_series: pd.Series = None,
-                          residuals_series: pd.Series = None):
-        if d_parameter_series is None:
-            current_parameter = self.parameter
-            for e in self.evaluators:
-                evaluation_result = e.evaluate()
-                if evaluation_result['failed']:
-                    return self.grad_kalman.grad, self.grad_kalman.cov, True
-                evaluation_result.drop(['failed'])
-                evaluated_parameters = evaluation_result.index.tolist()
-                for i in evaluated_parameters:
-                    if i != "residual":
-                        self.parameter[i] = evaluation_result[i]
-                self.parameter = self.parameter.sort_index()
+        assert len(self._target) == len(self._gradient_estimators)
 
-            d_parameter_series = self.parameter.add(-1.*current_parameter)
-        d_parameter_series = d_parameter_series.sort_index()
-        d_parameter_vector = np.asarray(d_parameter_series.values)
-        d_parameter_vector = d_parameter_vector.T
-        d_voltages_series = d_voltages_series.sort_index()
-        d_voltages_vector = np.asarray(d_voltages_series.values)
-        d_voltages_vector = d_voltages_vector.T
+    @property
+    def target(self) -> pd.Series:
+        return self._target
 
-        r = np.copy(self.grad_kalman.filter.R)
-        residuals = residuals_series.sort_index()
-        for j in range(self.parameter.size):
-            r[j, j] = 0.1 * r[j, j] + residuals[j]
+    @target.setter
+    def target(self, val):
+        self._target[:] = val
 
-        self.grad_kalman.update(d_voltages_vector, d_parameter_vector, measurement_covariance=r, hack=False)
-        self.gradient = self.grad_kalman.grad
-        return self.grad_kalman.grad, self.grad_kalman.cov, False
-
-
-class KalmanNewtonSolver(KalmanSolver):
-    """
-    Uses the Newton algorithm to compute the voltage steps and the Kalman filter to update the gradient.
-    """
-    def __init__(self, gate_names=None, gradient=None, evaluators: Tuple[Evaluator, ...] = (),
-                 desired_values: pd.Series = pd.Series(), covariance=None, noise=None, alpha=1.02,
-                 shifting_uncertainty=None):
-        super().__init__(gate_names=gate_names, gradient=gradient, evaluators=evaluators, desired_values=desired_values,
-                         covariance=covariance, noise=noise, alpha=alpha, shifting_uncertainty=shifting_uncertainty)
+    @property
+    def jacobian(self) -> pd.DataFrame:
+        gradients = [gradient.estimate() for gradient in self._gradient_estimators]
+        return pd.DataFrame(gradients, columns=self._target.index)
 
     def suggest_next_step(self) -> pd.Series:
-        if not self.check_dimensionality():
-            print('The internal dimensionality is not consistent! Cant predict next Step! Abort mission!')
-            return pd.Series()
-        d_parameter_series = self.desired_values.add(-1.*self.parameter)
-        d_parameter_series = d_parameter_series.sort_index()
-        d_parameter_vector = np.asarray(d_parameter_series.values).T
-        d_voltages_vector = np.linalg.lstsq(self.grad_kalman.grad, d_parameter_vector)[0]
-        d_voltages_series = pd.Series(d_voltages_vector, self.gate_names)
-        return d_voltages_series
+        for estimator in self._gradient_estimators:
+            suggestion = estimator.require_measurement()
+            if suggestion:
+                return suggestion
+        # our jacobian is sufficiently accurate
+        required_diff = self.target - self._current_position
+
+        step, *_ = np.linalg.lstsq(self.jacobian, required_diff)
+        return step
+
+    def update_after_step(self, voltages: pd.Series, parameters: pd.Series, variances: pd.Series):
+        for estimator, value, variance in zip(self._gradient_estimators, parameters, variances):
+            estimator.update(voltages, value, variance, is_new_position=True)
+        self._current_position[:] = parameters
+
+
+class NelderMeadSolver(Solver):
+    def __init__(self, target: pd.Series, simplex: Sequence[Tuple[pd.Series, pd.Series]], weights: pd.Series,
+                 current_voltages: pd.Series):
+        self.target = target
+        self.simplex = list(simplex)
+        self.weights = weights
+        self.current_voltages = current_voltages
+
+    def suggest_next_step(self) -> pd.Series:
+        if len(self.simplex) < len(self.current_voltages) + 1:
+            pass
+
+        def intercept(requested_point: np.ndarray):
+            for volts, params in self.simplex:
+                if all((requested_point - volts).abs() < 1e-10):
+                    return (params * self.weights).norm()**2
+            return 1e9
+
+        simplex = np.array([volts for volts, params in self.simplex])
+        options = dict(initial_simplex=simplex, maxfev=1)
+
+        results = scipy.optimize.minimize(intercept, x0=simplex[0], method='Nelder-Mead', options=options)
+
+        raise NotImplementedError('TODO')
+
