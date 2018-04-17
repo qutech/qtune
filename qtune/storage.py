c5cac92d6339291186dbe86ca464b97835cf1eb1
Simon Humpohl <simon.humpohl@rwth-aachen.de>
2018-04-17 11:26:32 +0200

    Add hdf5 storage framework

---
+++ qtune/storage.py
@@ -0,0 +1,180 @@
+import warnings
+from typing import Union
+
+import h5py
+import numpy as np
+import pandas as pd
+
+
+__all__ = ["serializables", "HDF5Serializable"]
+
+
+serializables = dict()
+
+def _get_dtype(arr):
+    if arr.dtype == 'O':
+        if all(isinstance(t, str) for t in arr.ravel()):
+            return h5py.special_dtype(vlen=str)
+    return arr.dtype
+
+class HDF5Serializable(type):
+    def __new__(mcs, name, bases, attrs):
+        if 'to_hdf5' not in attrs:
+            raise AttributeError('Missing method: "to_hdf5" that should return all constructor arguments')
+
+        if name in serializables:
+            warnings.warn("Overwriting known serializable", name)
+        cls = type.__new__(mcs, name, bases, attrs)
+
+        serializables[name] = cls
+        return cls
+
+
+def _to_hdf5(hdf5_parent_group: h5py.Group, name, obj, serialized):
+    if id(obj) in serialized:
+        hdf5_parent_group.create_dataset(name, data=serialized[id(obj)].ref)
+        return
+
+    if isinstance(obj, dict):
+        hdf5_group = hdf5_parent_group.create_group(name)
+        hdf5_group.attrs['#type'] = 'dict'
+        for key, value in obj.items():
+            _to_hdf5(hdf5_group, key, value, serialized)
+        serialized[id(obj)] = hdf5_group
+        return
+
+    if isinstance(obj, (list, tuple)):
+        hdf5_group = hdf5_parent_group.create_group(name)
+        hdf5_group.attrs['#type'] = 'list' if isinstance(obj, list) else 'tuple'
+        for idx, value in enumerate(obj):
+            _to_hdf5(hdf5_group, str(idx), value, serialized)
+        serialized[id(obj)] = hdf5_group
+        return
+
+    if type(obj).__name__ in serializables:
+        hdf5_group = hdf5_parent_group.create_group(name)
+        hdf5_group.attrs['#type'] = type(obj).__name__
+
+        serialized[id(obj)] = hdf5_group
+
+        data = obj.to_hdf5()
+        for key, value in data.items():
+            _to_hdf5(hdf5_group, key, value, serialized)
+        return
+
+    if isinstance(obj, pd.DataFrame):
+        dset = hdf5_parent_group.create_dataset(name, data=obj)
+        dset.attrs.create('index', data=obj.index, dtype=_get_dtype(obj.index))
+        dset.attrs.create('columns', data=obj.columns, dtype=_get_dtype(obj.columns))
+        dset.attrs['#type'] = 'DataFrame'
+
+        serialized[id(obj)] = dset
+        return
+
+    if isinstance(obj, pd.Series):
+        dset = hdf5_parent_group.create_dataset(name, data=obj)
+        dset.attrs.create('index', data=obj.index, dtype=_get_dtype(obj.index))
+        dset.attrs['#type'] = 'Series'
+
+        serialized[id(obj)] = dset
+        return
+
+    if isinstance(obj, np.ndarray):
+        dset = hdf5_parent_group.create_dataset(name, data=obj)
+        serialized[id(obj)] = dset
+        return
+
+    if isinstance(obj, (float, int, complex)):
+        hdf5_parent_group.create_dataset(name, data=obj, shape=())
+        return
+
+    raise RuntimeError()
+
+
+def to_hdf5(filename_or_handle: Union[str, h5py.Group], name: str, obj):
+    if isinstance(filename_or_handle, h5py.Group):
+        root = filename_or_handle
+    else:
+        root = h5py.File(filename_or_handle, mode='r+')
+
+    _to_hdf5(root, name, obj, dict())
+
+
+def _from_hdf5(root: h5py.File, hdf5_obj: h5py.HLObject, deserialized=None):
+    if isinstance(hdf5_obj, h5py.Reference):
+        hdf5_obj = root[hdf5_obj]
+
+    if hdf5_obj.id in deserialized:
+        return deserialized[hdf5_obj.id]
+
+    if isinstance(hdf5_obj, h5py.Group):
+        if '#type' not in hdf5_obj.attrs or hdf5_obj.attrs['#type'] == 'dict':
+            deserialized[hdf5_obj.id] = dict()
+            result = deserialized[hdf5_obj.id]
+
+            for k in hdf5_obj.keys():
+                result[k] = _from_hdf5(root, hdf5_obj[k], deserialized)
+            return result
+
+        elif hdf5_obj.attrs['#type'] in serializables:
+            deserialized[hdf5_obj.id] = hdf5_obj.attrs['#type'](
+                **{k: _from_hdf5(root, v, deserialized)
+                   for k, v in hdf5_obj.items()}
+            )
+            return deserialized[hdf5_obj.id]
+
+        elif hdf5_obj.attrs['#type'] == 'list':
+            deserialized[hdf5_obj.id] = list()
+            result = deserialized[hdf5_obj.id]
+
+            for idx in range(len(hdf5_obj.keys())):
+                result.append(_from_hdf5(root, hdf5_obj[str(idx)], deserialized))
+            return result
+
+        elif hdf5_obj.attrs['#type'] == 'tuple':
+            result = []
+            for idx in range(len(hdf5_obj.keys())):
+                result.append(_from_hdf5(root, hdf5_obj[str(idx)], deserialized))
+            result = tuple(result)
+            deserialized[hdf5_obj.id] = result
+            return result
+
+
+        else:
+            warnings.warn('Unknown type: ', hdf5_obj.attrs['#type'])
+
+    elif isinstance(hdf5_obj, h5py.Dataset):
+        if '#type' in hdf5_obj.attrs:
+            if hdf5_obj.attrs['#type'] == 'DataFrame':
+                idx = hdf5_obj.attrs['index']
+                col = hdf5_obj.attrs['columns']
+                result = pd.DataFrame(data=np.asarray(hdf5_obj), index=idx, columns=col)
+                deserialized[hdf5_obj.id] = result
+                return result
+
+            if hdf5_obj.attrs['#type'] == 'Series':
+                idx = hdf5_obj.attrs['index']
+                result = pd.Series(data=np.asarray(hdf5_obj), index=idx)
+                deserialized[hdf5_obj.id] = result
+                return result
+
+
+        result = np.asarray(hdf5_obj)
+        if result.shape == ():
+            result = result[()]
+        if isinstance(result, h5py.Reference):
+            return _from_hdf5(root, result, deserialized)
+        else:
+            return result
+
+    else:
+        raise RuntimeError()
+
+
+def from_hdf5(filename_or_handle):
+    if isinstance(filename_or_handle, h5py.Group):
+        root = filename_or_handle
+    else:
+        root = h5py.File(filename_or_handle, mode='r')
+
+    return _from_hdf5(root, root, dict())
---
+++ tests/test_storage.py
@@ -0,0 +1,81 @@
+import unittest
+import tempfile
+import os
+
+import numpy as np
+import pandas as pd
+
+from qtune.storage import HDF5Serializable, to_hdf5, from_hdf5
+
+
+class SerializationTests(unittest.TestCase):
+    def setUp(self):
+        self.temp_file = tempfile.NamedTemporaryFile(suffix='.hdf5', delete=False)
+        self.temp_file.close()
+
+    def tearDown(self):
+        os.remove(self.temp_file.name)
+
+    def test_pure_python_serialization(self):
+        data = {'asd': [1, 2, 3],
+                'ggg': [{'a': 1}, [1, 2, 3], (1, 2, 3)]}
+
+        to_hdf5(self.temp_file.name, 'data', data)
+
+        recovered_data = from_hdf5(self.temp_file.name)
+
+        self.assertEqual({'data': data}, recovered_data)
+
+    def test_referenced_object_serialization(self):
+        l_1 = [8, 7, 6]
+        data = {'asd': [1, 2, 3],
+                'ggg': [{'a': 1}, [1, 2, 3], l_1],
+                '9000': l_1}
+
+        to_hdf5(self.temp_file.name, 'data', data)
+
+        recovered_data = from_hdf5(self.temp_file.name)
+
+        self.assertEqual({'data': data}, recovered_data)
+
+        self.assertIs(recovered_data['data']['9000'], recovered_data['data']['ggg'][2])
+
+    def test_numpy_serialization(self):
+        data = {'asd': [1, 2, 3],
+                'ggg': [{'a': 1}, [1, 2, 3], np.arange(2, 9)]}
+
+        to_hdf5(self.temp_file.name, 'data', data)
+
+        recovered_data = from_hdf5(self.temp_file.name)
+
+        np.testing.assert_equal({'data': data}, recovered_data)
+
+    def test_data_frame_serialization(self):
+        df = pd.DataFrame(data=[[1, 2, 3], [4, 5, 6]], index=['asdf', 'b'], columns=[0, 1, 2])
+        data = {'asd': [1, 2, 3],
+                'ggg': [{'a': 1}, [1, 2, 3], df]}
+
+        to_hdf5(self.temp_file.name, 'data', data)
+
+        recovered_data = from_hdf5(self.temp_file.name)
+
+        pd.testing.assert_frame_equal(recovered_data['data']['ggg'][2], data['ggg'][2])
+
+        recovered_data['data']['ggg'].pop(2)
+        data['ggg'].pop(2)
+        self.assertEqual({'data': data}, recovered_data)
+
+    def test_series_serialization(self):
+        ser = pd.Series(data=[1., 53, 95.4], index=['asdf', 'b', 'hh'])
+        data = {'asd': [1, 2, 3],
+                'ggg': [{'a': 1}, [1, 2, 3], ser]}
+
+        to_hdf5(self.temp_file.name, 'data', data)
+
+        recovered_data = from_hdf5(self.temp_file.name)
+
+        pd.testing.assert_series_equal(recovered_data['data']['ggg'][2], data['ggg'][2])
+
+        recovered_data['data']['ggg'].pop(2)
+        data['ggg'].pop(2)
+        self.assertEqual({'data': data}, recovered_data)
