69bb53b7f311493ae912be0558cf92711abbfe37
INSTITUT2B\teske <julian.teske@rwth-aachen.de>
2018-05-08 12:47:30 +0200

    WIP implementation of reader

--- qtune/reader.py
+++ qtune/reader.py
@@ -9,11 +9,11 @@ class Reader:
         self._file_path = path
         self.tuner_list, self.index_list = self.load_data()
 
-        voltage_list = []
-        paramater_list = []
+        self.voltage_list = []
+        self.parameter_list = []
         for tuning_hierarchy in self.tuner_list:
-            voltage_list.append(extract_voltages_from_hierarchy(tuning_hierarchy))
-            paramater_list.append(extract_parameters_from_hierarchy(tuning_hierarchy))
+            self.voltage_list.append(extract_voltages_from_hierarchy(tuning_hierarchy))
+            self.parameter_list.append(extract_parameters_from_hierarchy(tuning_hierarchy))
 
 
     def load_data(self):
@@ -27,7 +27,8 @@ class Reader:
         index_list = []
         for file in directory_content:
             hdf5_handle = h5py.File(self._file_path + r"\\" + file, mode="r")
-            autotuner = qtune.storage.from_hdf5(hdf5_handle, reserved={"experiment": None})
+            loaded_data = qtune.storage.from_hdf5(hdf5_handle, reserved={"experiment": None})
+            autotuner = loaded_data["autotuner"]
             assert(isinstance(autotuner, qtune.autotuner.Autotuner))
 
             tuner_list.append(autotuner._tuning_hierarchy)
@@ -38,14 +39,16 @@ class Reader:
 def extract_voltages_from_hierarchy(tuning_hierarchy):
     voltages = pd.Series()
     for par_tuner in tuning_hierarchy:
-        for gate in par_tuner._last_voltage.index():
-            if gate not in voltages.index():
+        for gate in pd.Series(par_tuner._last_voltage).index:
+            if gate not in voltages.index:
                 voltages[gate] = par_tuner._last_voltage[gate]
     return voltages
 
 
 def extract_parameters_from_hierarchy(tuning_hierarchy):
     parameters = pd.Series()
-    parameters.append(par_tuner._last_parameter_values for par_tuner in tuning_hierarchy)
+    for par_tuner in tuning_hierarchy:
+        parameters.append(pd.Series(par_tuner._last_parameter_values))
+    return parameters
 
 #def extract_gradients_from_hierarchy
