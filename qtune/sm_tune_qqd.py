ac1b9eeda85eb29fc4363cd67661c19be56c6a90
INSTITUT2B\teske <julian.teske@rwth-aachen.de>
2018-08-08 19:21:16 +0200

    implementation of evaluators for the triton setup

--- qtune/sm_tune_qqd.py
+++ qtune/sm_tune_qqd.py
@@ -8,6 +8,7 @@ from qtune.experiment import Experiment, Measurement
 from qtune.evaluator import Evaluator
 from qtune.sm import SpecialMeasureMatlab
 from qtune import mat2py
+from qtune.history import History
 
 # This file bundles everything connected to the QQD
 # Is this the file structure we want?
@@ -67,9 +68,6 @@ class SMTuneQQD(Experiment):
 
     def read_gate_voltages(self) -> pd.Series:
         return pd.Series(self._matlab.engine.qtune.read_qqd_gate_voltages()).sort_index()
-
-    def get_last_file_name(self):
-        return self._last_file_name
 
     def set_gate_voltages(self, new_gate_voltages: pd.Series) -> pd.Series:
 
@@ -136,52 +134,20 @@ class SMTuneQQD(Experiment):
         # TODO allow this to create the measurement on the fly when arguments cntrl, index, options are passed
         if measurement.name not in self._measurements.keys():
             raise ValueError(f'Unknown measurement: {measurement}')
+
         for key in measurement.options:
             if isinstance(measurement.options[key], np.generic):
                 measurement.options[key] = self._matlab.to_matlab(measurement.options[key])
 
         result = self.pytune(measurement)
-        # check if this is saved correctly
-        self._last_file_name = result['data'].args.fullFile
 
-        if measurement.name == 'line':
-            width = np.full(len(result['data'].ana), np.nan)
-            for i in range(len(result['data'].ana)):
-                width[i] = result['data'].ana[i].width
-                # variance[i] = result['data'].ana[i].sumResiduals
-            ret = np.array([np.mean(width), np.var(width) / len(result['data'].ana)])
-        elif measurement.name == 'lead':
-            ret = np.array([result['data'].ana.fitParams[1][3], result['data'].ana.sumResiduals])
-        elif measurement.name == 'load':
+        if measurement.name in ['line', 'lead', 'load', 'load pos', 'chrg rnd', 'chrg s', 'sensor', 'sensor 2d', 'chrg',
+                                'resp']:
             ret = result
-        elif measurement.name == 'load pos':
-            ret = result
-        elif measurement.name == 'chrg rnd':
-            ret = result
-        elif measurement.name == 'chrg s':
-            ret = result
-        elif measurement.name == 'sensor':
-            ret = np.array([result['data'].ana.xVal, np.abs(result['data'].ana.minGradData)])
-        elif measurement.name == 'sensor 2d':
-            ret = np.array([result['data'].ana.xVal, result['data'].ana.yVal, np.abs(result['data'].ana.minGradData)])
-        elif measurement.name == 'chrg':
-            if measurement.options['index'] == 2:
-                ret = np.array([np.linalg.norm(np.array(result['data'].ana.triplePointRight) -
-                                 np.array(result['data'].ana.triplePointLeft))]
-
-                               ).astype(float)
-            else:
-                ret = np.squeeze(result['data'].ana.O)
         elif measurement.name == 'stp':
             ret = np.array([result['data'].ana.STp_x, result['data'].ana.STp_y])
         elif measurement.name == 'tl':
             ret = np.array([result['data'].ana.Tp_x, result['data'].ana.Tp_y])
-        elif measurement.name == 'resp':
-            n = len(result['data'].ana)
-            ret = np.full(2 * n, np.nan)
-            for i in range(n):
-                ret[i] = result['data'].ana[i].position
-                ret[n + i] = result['data'].ana[i].sumResiduals
         else:
             raise ValueError(f'Measurement {measurement} not implemented')
 
@@ -290,3 +256,209 @@ class SMQQDPassThru(Evaluator):
                     last_file_names=self._last_file_names,
                     error=self._error,
                     reference_residual_sum=self._reference_residual_sum)
+
+class QQDLine(Evaluator):
+    def __init__(self, experiment: SMTuneQQD, measurements: List[Measurement],
+                 parameters: List[str], name: str, raw_x_data=tuple(), raw_y_data=tuple(), last_file_names=None):
+
+        super().__init__(experiment, measurements, parameters, raw_x_data, raw_y_data, name=name)
+        self._count = count(0)
+        self._last_file_names = last_file_names
+
+    def evaluate(self):
+        self.logger.info(f'Evaluating {self.parameters}.')
+        measurement_result = self.experiment.measure(self.measurements[0])
+        value, variance = self.process_raw_data(measurement_result)
+        return pd.Series(data=value, index=[self.parameters[0]]), pd.Series(data=variance, index=[self.parameters[0]])
+
+    def process_raw_data(self, raw_data):
+        """
+        This method averages the evaluations and takes the Variances.
+        :param raw_data: return of measurement (Matlab struct)
+        :return: parameter estimate and its estimated variance
+        """
+        width = np.full(len(raw_data['data'].ana), np.nan)
+        for i in range(len(raw_data['data'].ana)):
+            width[i] = raw_data['data'].ana[i].width
+        value = np.mean(width)
+        variance = np.var(width) / len(raw_data['data'].ana)
+        self._last_file_names = raw_data['data'].args.fullFile
+        return value, variance
+
+    def to_hdf5(self):
+        return dict(super().to_hdf5(),
+                    last_file_names=self._last_file_names)
+
+class QQDLead(Evaluator):
+    def __init__(self, experiment: SMTuneQQD, measurements: List[Measurement],
+                 parameters: List[str], name: str, raw_x_data=tuple(), raw_y_data=tuple(), last_file_names=None,
+                 error: float=1, reference_residual_sum: float=1.):
+
+        super().__init__(experiment, measurements, parameters, raw_x_data, raw_y_data, name=name)
+        self._error = error
+        self._reference_residual_sum = reference_residual_sum
+        self._last_file_names = last_file_names
+
+    def evaluate(self) -> Tuple[pd.Series, pd.Series]:
+        self.logger.info(f'Evaluating {self.parameters}.')
+        measurement_result = self.experiment.measure(self.measurements[0])
+        value, variance = self.process_raw_data(measurement_result)
+        return pd.Series(data=value, index=[self.parameters[0]]), pd.Series(data=variance, index=[self.parameters[0]])
+
+    def process_raw_data(self, raw_data):
+        value = raw_data['data'].ana.fitParams[1][3]
+        sum_residuals = raw_data['data'].ana.sumResiduals
+        self._last_file_names = raw_data['data'].args.fullFile
+        return value, self._error * sum_residuals / self._reference_residual_sum
+
+    def to_hdf5(self):
+        return dict(super().to_hdf5(),
+                    last_file_names=self._last_file_names,
+                    error=self._error,
+                    reference_residual_sum=self._reference_residual_sum)
+
+
+class QQDSensor1D(Evaluator):
+    def __init__(self, experiment: SMTuneQQD, measurements: List[Measurement],
+                 parameters: List[str], name: str, raw_x_data=tuple(), raw_y_data=tuple(), last_file_names=None):
+        super().__init__(experiment, measurements, parameters, raw_x_data, raw_y_data, name=name)
+        self._last_file_names = last_file_names
+
+    def evaluate(self) -> Tuple[pd.Series, pd.Series]:
+        self.logger.info(f'Evaluating {self.parameters}.')
+        measurement_result = self.experiment.measure(self.measurements[0])
+        position, slope = self.process_raw_data(measurement_result)
+        return pd.Series(data=[position, slope], index=[self.parameters]), pd.Series(data=[np.nan, np.nan],
+                                                                                     index=[self.parameters])
+
+    def process_raw_data(self, raw_data):
+        position = raw_data['data'].ana.xVal
+        slope = np.abs(raw_data['data'].ana.minGradData)
+        self._last_file_names = raw_data['data'].args.fullFile
+        return position, slope
+
+    def to_hdf5(self):
+        return dict(super().to_hdf5(),
+                    last_file_names=self._last_file_names)
+
+
+class QQDSensor2D(Evaluator):
+    def __init__(self, experiment: SMTuneQQD, measurements: List[Measurement],
+                 parameters: List[str], name: str, raw_x_data=tuple(), raw_y_data=tuple(), last_file_names=None):
+        super().__init__(experiment, measurements, parameters, raw_x_data, raw_y_data, name=name)
+        self._last_file_names = last_file_names
+
+    def evaluate(self) -> Tuple[pd.Series, pd.Series]:
+        self.logger.info(f'Evaluating {self.parameters}.')
+        measurement_result = self.experiment.measure(self.measurements[0])
+        position_x, position_y, slope = self.process_raw_data(measurement_result)
+        return pd.Series(data=[position_x, position_y, slope], index=[self.parameters]), \
+               pd.Series(data=[np.nan, np.nan, np.nan], index=[self.parameters])
+
+    def process_raw_data(self, raw_data):
+        position_x = raw_data['data'].ana.xVal
+        position_y = raw_data['data'].ana.yVal
+        slope = np.abs(raw_data['data'].ana.minGradData)
+        self._last_file_names = raw_data['data'].args.fullFile
+        return position_x, position_y, slope
+
+    def to_hdf5(self):
+        return dict(super().to_hdf5(),
+                    last_file_names=self._last_file_names)
+
+
+class QQDCharge(Evaluator):
+    def __init__(self, experiment: SMTuneQQD, measurements: List[Measurement],
+                 parameters: List[str], name: str, raw_x_data=tuple(), raw_y_data=tuple(), last_file_names=None,
+                 error=1):
+        super().__init__(experiment, measurements, parameters, raw_x_data, raw_y_data, name=name)
+        self._last_file_names = last_file_names
+        self._error = error
+
+    def evaluate(self) -> Tuple[pd.Series, pd.Series]:
+        self.logger.info(f'Evaluating {self.parameters}.')
+        measurement_result = self.experiment.measure(self.measurements[0])
+        positions, errors = self.process_raw_data(measurement_result)
+        return pd.Series(data=positions, index=self.parameters), \
+               pd.Series(data=errors, index=self.parameters)
+
+    def process_raw_data(self, raw_data):
+        positions = np.squeeze(raw_data['data'].ana.O)
+        errors = np.full(shape=positions.shape, fill_value=self._error)
+        self._last_file_names = raw_data['data'].args.fullFile
+        return positions, errors
+
+    def to_hdf5(self):
+        return dict(super().to_hdf5(),
+                    last_file_names=self._last_file_names,
+                    error=self._error)
+
+
+class QQDDistTP(Evaluator):
+    def __init__(self, experiment: SMTuneQQD, measurements: List[Measurement],
+                 parameters: List[str], name: str, raw_x_data=tuple(), raw_y_data=tuple(), last_file_names=None,
+                 error=1):
+        super().__init__(experiment, measurements, parameters, raw_x_data, raw_y_data, name=name)
+        self._last_file_names = last_file_names
+        self._error = error
+
+    def evaluate(self) -> Tuple[pd.Series, pd.Series]:
+        self.logger.info(f'Evaluating {self.parameters}.')
+        measurement_result = self.experiment.measure(self.measurements[0])
+        distance, errors = self.process_raw_data(measurement_result)
+        return pd.Series(data=distance, index=self.parameters), \
+               pd.Series(data=errors, index=self.parameters)
+
+    def process_raw_data(self, raw_data):
+        distance = np.linalg.norm(
+            np.array(raw_data['data'].ana.triplePointRight) - np.array(raw_data['data'].ana.triplePointLeft))
+        self._last_file_names = raw_data['data'].args.fullFile
+        return distance, self._error
+
+    def to_hdf5(self):
+        return dict(super().to_hdf5(),
+                    last_file_names=self._last_file_names,
+                    error=self._error)
+
+class QQDResp(Evaluator):
+    def __init__(self, experiment: SMTuneQQD, measurements: List[Measurement],
+                 parameters: List[str], name: str, raw_x_data=tuple(), raw_y_data=tuple(), last_file_names=None,
+                 error=1):
+        super().__init__(experiment, measurements, parameters, raw_x_data, raw_y_data, name=name)
+        self._last_file_names = last_file_names
+        self._error = pd.Series(data=error, index=self.parameters)
+
+    def evaluate(self) -> Tuple[pd.Series, pd.Series]:
+        self.logger.info(f'Evaluating {self.parameters}.')
+        measurement_result = self.experiment.measure(self.measurements[0])
+        positions, errors = self.process_raw_data(measurement_result)
+        return pd.Series(data=positions, index=self.parameters), \
+               pd.Series(data=errors, index=self.parameters)
+
+    def process_raw_data(self, raw_data):
+        n = len(raw_data['data'].ana)
+        positions = np.full(n, np.nan)
+        for i in range(n):
+            positions[i] = raw_data['data'].ana[i].position
+        self._last_file_names = raw_data['data'].args.fullFile
+        return positions, self._error
+
+    def to_hdf5(self):
+        return dict(super().to_hdf5(),
+                    last_file_names=self._last_file_names,
+                    error=self._error)
+
+
+def reevaluate_data_with_matlab(experiment, history: History, evaluator_name, indices, hold=True):
+    if isinstance(indices, int):
+        indices = [indices, ]
+    results = []
+    for i in indices:
+        evaluator_data = history._evaluator_data[evaluator_name].iloc[i]
+        measurement = evaluator_data.measurements[0]
+        measurement.options['loadFile'] = evaluator_data._last_file_names
+        results.append(experiment.measure(measurement))
+        if hold:
+            _ = input('Type anything to continue.')
+    return results
+
